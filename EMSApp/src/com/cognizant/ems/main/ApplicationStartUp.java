6f358e333faa910e7292de8f96b8dc0217b41c06
Yalagooradappa1 <yalagureshsh1998@gmail.com>
2021-03-22 12:28:23 +0530

    EAS APP ADDED

---
+++ EMSApp/src/com/cognizant/ems/main/ApplicationStartUp.java
@@ -0,0 +1,13 @@
+package com.cognizant.ems.main;
+
+import com.cognizant.ems.util.MenuUtil;
+
+public class ApplicationStartUp {
+
+	public static void main(String[] args) {
+
+		MenuUtil menuUtil = new MenuUtil();
+		menuUtil.start();
+	}
+
+}
